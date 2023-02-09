e2530c36d72720951fc2c1a3b85810e2795b7f7c
Shivam-officials <suryabhardwajofficial@gmail.com>
2023-02-09 11:37:18 +0530

    added the text and button in wellnessScreen

--- app/src/main/java/com/example/basicsstatecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicsstatecodelab/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.basicsstatecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicsstatecodelab.ui.theme.BasicsStateCodeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,22 +26,52 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
     }
 }
 
+
+
+
+
+// composable for water-counter
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WaterCounter(modifier: Modifier = Modifier){
+
+    Column(modifier = modifier.padding(16.dp))
+    {
+        var count = 0
+        //text for showing number of waterCounter
+        Text(
+            text = "you've  had $count glasses.",
+        )
+        Button(
+            onClick = { count++ },
+            modifier = modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
+// preview for waterCounter
+//@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview(){
+    WaterCounter( )
 }
 
+//Composable for wellnessScreen
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier){
+    WaterCounter(modifier)
+}
+
+//Preview of WellnessScreen
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    BasicsStateCodeLabTheme {
-            Greeting("Android ")
-    }
+fun WellnessScreenPreview(){
+    WellnessScreen()
 }
