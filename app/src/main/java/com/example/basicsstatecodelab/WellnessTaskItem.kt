fc38beba064d8e0820ae1b17ffc7e73596152ec6
Shivam-officials <suryabhardwajofficial@gmail.com>
2023-02-13 13:22:56 +0530

    .

--- app/src/main/java/com/example/basicsstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/com/example/basicsstatecodelab/WellnessTaskItem.kt
@@ -8,13 +8,11 @@ import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 
 
 //stateless WellnessTaskItem
